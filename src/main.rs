a97118b8e527c247f1c6f28f505daf82cac9bbf2
HookedBehemoth <luisscheurenbrand@gmail.com>
2022-10-20 20:24:00 +0200

    use json api backend

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 use std::process::exit;
 
 use argh::FromArgs;
-use itertools::Itertools;
-use scraper::{ElementRef, Selector};
+use serde::Deserialize;
 
-const HLTB_URL: &str = "https://howlongtobeat.com/search_results";
+const HLTB_URL: &str = "https://howlongtobeat.com/api/search";
 
 #[derive(Debug, FromArgs)]
 /// hltb only really only needs the game argument
@@ -24,105 +23,85 @@ fn main() {
         exit(1)
     }
 
-    let game = args.game.join(" ");
+    let doc = search(&args);
 
-    let doc = search(&game);
-
-    match parse_result_amount(&doc) {
-        Some(amount) => {
-            let s = if amount == "1" { "" } else { "s" };
-            println!("Found {} matching game{}\n", amount, s);
-        }
-        None => {
-            println!("Could not find any games for query '{}'", game);
+    match doc.data.len() {
+        0 => {
+            println!("Could not find any games");
             exit(1);
+        }
+        amount => {
+            let s = if amount == 1 { "" } else { "s" };
+            println!("Found {} matching game{}", amount, s);
         }
     }
 
-    let games = parse_games(&doc, args.number);
-    games.iter().for_each(|g| print_game(&g));
-}
-
-#[derive(Debug)]
-struct Game {
-    name: String,
-    entries: Vec<(String, String)>,
-}
-
-fn search(game: &str) -> scraper::Html {
-    let form_data = &[
-        ("queryString", game),
-        ("t", "games"),
-        ("sorthead", "popular"),
-    ];
-
-    let res = ureq::post(HLTB_URL)
-        .query("page", "1")
-        .set("Referer", "https://howlongtobeat.com")
-        .send_form(form_data)
-        .expect("Request to howlongtobeat.com failed")
-        .into_string()
-        .expect("Could not read response");
-
-    scraper::Html::parse_document(&res)
-}
-
-fn parse_result_amount(doc: &scraper::Html) -> Option<String> {
-    let selector_games_number = Selector::parse("h3").unwrap();
-    return match doc.select(&selector_games_number).next() {
-        Some(games_number) => {
-            // Get the number of games from the sentence `We Found 25 Games for \"gris\"`
-            games_number
-                .inner_html()
-                .as_str()
-                .split_whitespace()
-                .skip(2)
-                .next()
-                .map(str::to_string)
-        }
-        None => None,
-    };
-}
-
-fn parse_games(doc: &scraper::Html, n: Option<usize>) -> Vec<Game> {
-    let selector_list_item = Selector::parse(".search_list_details").unwrap();
-    doc.select(&selector_list_item)
-        .take(n.unwrap_or(5))
-        .map(parse_game)
-        .collect()
-}
-
-fn parse_game(item: ElementRef) -> Game {
-    let selector_title = Selector::parse("a").unwrap();
-    let selector_time =
-        Selector::parse(".search_list_tidbit, .search_list_tidbit_short, .search_list_tidbit_long")
-            .unwrap();
-
-    let mut name = String::new();
-    html_escape::decode_html_entities_to_string(
-        &item
-            .select(&selector_title)
-            .next()
-            .expect("Could not find game title")
-            .inner_html(),
-        &mut name,
+    let mut data = doc.data;
+    data.sort_by(
+        |lhs, rhs|
+        rhs.comp_all_count.cmp(&lhs.comp_all_count)
     );
-
-    let times = item.select(&selector_time);
-    let entries = times
-        .map(|e| e.inner_html().trim().into())
-        .tuples::<(String, String)>()
-        .collect();
-
-    Game { name, entries }
+    data
+        .iter()
+        .take(args.number.unwrap_or(5))
+        .for_each(print_game);
 }
 
-fn print_game(game: &Game) {
-    let entries: String = game
-        .entries
-        .iter()
-        .map(|e| format!("{}: {}\n", e.0, e.1))
-        .collect();
-    let bold_name = format!("\x1B[1m{}\x1B[0m", game.name);
-    println!("{}\n{}", bold_name, entries);
+#[derive(Deserialize)]
+struct SearchResponse {
+    data: Vec<SearchData>,
+}
+
+#[derive(Deserialize)]
+struct SearchData {
+    game_name: String,
+    comp_main: usize,
+    comp_plus: usize,
+    comp_100: usize,
+    comp_all_count: usize,
+    release_world: usize,
+}
+
+fn search(args: &Args) -> SearchResponse {
+    let request = ureq::json!({
+        "searchPage": 1,
+        "searchType": "games",
+        "searchTerms": &args.game,
+        "size": 20
+    });
+
+    ureq::post(HLTB_URL)
+        .set("Referer", "https://howlongtobeat.com/")
+        .send_json(ureq::json!(request))
+        .expect("Request to howlongtobeat.com failed")
+        .into_json::<SearchResponse>()
+        .expect("Could not read response")
+}
+
+fn print_game(game: &SearchData) {
+    println!("\n\x1B[1m{} ({})\x1B[0m", game.game_name, game.release_world);
+    [
+        ("Main Story", game.comp_main),
+        ("Main + Extra", game.comp_plus),
+        ("Completionist", game.comp_100),
+    ]
+    .into_iter()
+    .for_each(|(n, t)| println!("{n}: {}", seconds_to_string(t)));
+}
+
+fn seconds_to_string(time: usize) -> String {
+    if time == 0 {
+        return "--".to_owned()
+    }
+    let mut hours = time / 3600;
+    let minutes = (time / 60) % 60;
+    let fraction = if hours < 100 && minutes > 15 && minutes < 45 {
+        "½"
+    } else {
+        if minutes >= 45 {
+            hours += 1;
+        }
+        ""
+    };
+    format!("{}{} Hours", hours, fraction)
 }
